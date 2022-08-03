e03d959b39bfa46ca9f4f0e500622766cd234c20
Christian Espinoza <christian.espinoza1008@gmail.com>
2022-08-03 15:36:59 -0500

    Add solved exercise and comment in README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Regex
 Course files for the Regular Expressions playlist on The Net Ninja YouTube channel.
+
+## Acknowledgements
+ - [Regular Expressions (RegEx) Tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9g6m_6Sld9Q4jzqdqHd2HiD)
+ - [Regular Expression 101 playground](https://regex101.com/)
+
+## Lessons Learned
+
+-  Opacity doesn't hide completely all paragraphs. I tried to copy and paste everything and get all hiden text.
--- validation.js
+++ validation.js
@@ -1,1 +1,25 @@
 // validation script here
+const inputs = document.querySelectorAll('input');
+const patterns = {}
+patterns.telephone = /^\d{11}$/;
+patterns.username = /^[a-z\d]{5,12}$/i;
+patterns.password = /^[\w@-]{8,20}$/;
+patterns.slug = /^[a-z\d-]{8,20}$/;
+patterns.email = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+
+// validation function
+const validate = (field, regex) => regex.test(field);
+
+inputs.forEach(input => {
+    input.addEventListener('keyup', (e) => {
+        // console.log(e.target.value); 
+        console.log(e.target.classList)
+        if (validate(e.target.value, patterns[e.target.name])) {
+            e.target.classList.add('valid');
+            e.target.classList.remove('invalid');
+        } else {
+            e.target.classList.remove('valid');
+            e.target.classList.add('invalid');
+        }
+    })
+});
